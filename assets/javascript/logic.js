659e2550b4ed7cb7d45953e3041a07251b766bf7
ryteone1 <rynlacy@yahoo.com>
2017-09-19 10:52:19 -0400

    updated with comments

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,14 +1,27 @@
+// I used the train scheduler code just as a base to try to get the code working.  The train scheduler code 
+// had a nice table that I was hoping to get the search results to display in.  The thought was that once the code was working we 
+// could integrate it into the bootstrap code that Joe found. Whatever html theme we do decide to use, we will need the following:
+// - a search form at the top to search events. 
+// - a table or div area to display the results below the search form.  We can use Ticketmaster or Eventful as a reference
+// - perhaps the search results could take up 8 out of the 12 columns to the left; Leaving an open area to the right to 
+//      display Gifs.  Pretty much, it would be like the size of an ad area to the right that many websites have
+// - the gifs will be playing on the right, while the user can view the search results on the left.
 
-// $(document).on("click", function() {
-// 	var person = $(this).html();
+$(document).ready(function() {
 
-
-// WORKING TICKETMASTER URL AND CODE
-
+// clicking submit on a new search should execute a function that will allow us to capture the search term and store it in a variable
+// var person.
 $("#search-events").on("click", function(event) {
   event.preventDefault();
 
+// Need var person should be equal to the search term the user entered.  We should then be able to integrate var person into the 
+// queryURL so that the user can search for new events. THIS IS CURRENTLY NOT WORKING.  I CAN GET IT TO WORK IF I 
+// MANUALLY SET VAR PERSON.  IF YOU COMMENT OUT var person = $(this).html();
+// AND UNCOMMENT var person = "shakira", you will see that the proper results get displayed.
+
   var person = $(this).html();
+
+  // var person = "shakira";
   console.log(this);
 	var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=NdH7ttoqHEznKuGMVdBINJqbG8r9w1Kk&keyword=" + person;
 
@@ -24,7 +37,7 @@ $.ajax({
       
       // var eventList = $("#event-list");
       var events = response._embedded.events;  
-         
+
       $("#event-name").empty();
       for (var i = 0; i < events.length; i++) {      
         
@@ -48,10 +61,12 @@ $.ajax({
      
     });
 
+});
+
 // END OF WORKING CODE FOR TICKETMASTER
 
     
-
+// TRIVIA DATABASE API
 // TRIVIA DATABASE - WAS WORKING BUT NOW ARRAY WONT DISPLAY
 //     var queryURL = "https://opentdb.com/api.php?amount=10&category=26&difficulty=easy";
 
@@ -70,17 +85,8 @@ $.ajax({
 // END OF TRIVIA DATABASE
 
 
-  
-    
 
-
-
-
-
-
-
-
-
+// SPOTIFY API
 //  var queryURL = "https://api.spotify.com/v1/albums?ids=382ObEPsp2rxGrnsizN5TX,1A2GTWGtFfWp7KSQTwWOyo,2noRn2Aes5aoNVsU6iWThc&market=ES HTTP/1.1"
 //  person = "james+bay";
 
